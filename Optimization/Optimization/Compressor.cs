79308837f4285cc2bb0cc7e485c2c0dafa36701b
Clarice Bouwer <clarice.bouwer@dstvdm.com>
2018-04-07 22:17:00 +0200

    Try a different approach with Compressor

--- Optimization/Optimization/Compressor.cs
+++ Optimization/Optimization/Compressor.cs
@@ -19,29 +19,32 @@ namespace Optimization
         {
             var total = 0;
             var output = new StringBuilder();
-            for (var index = 0; index < input.Length; index++)
+            for (var i = 0; i < input.Length; i++)
             {
-                var currentCharacter = input[index].ToString();
-                var nextCharacter = GetNextCharacter(input, index);
-                total += 1;
-                if (nextCharacter != currentCharacter)
-                {
-                    var compressedCouple = GetCompressedCouple(currentCharacter, total);
-                    output.Append(compressedCouple);
-                    total = 0;
-                }
+                var iteration = GetNextIteration(input, i, total);
+                total = iteration.total;
+                output.Append(iteration.output);
             }
             return output.ToString();
+        }
+
+        private static (string output, int total) GetNextIteration(string input, int index, int total)
+        {
+            var current = input[index].ToString();
+            var next = GetNextCharacter(input, index);
+            var output = string.Empty;
+            total += 1;
+            if (next != current)
+            {
+                output = $"{current}{total}";
+                total = 0;
+            }
+            return (output, total);
         }
 
         private static string GetNextCharacter(string input, int index)
         {
             return index + 1 < input.Length ? input[index + 1].ToString() : string.Empty;
-        }
-
-        private static string GetCompressedCouple(string character, int total)
-        {
-            return $"{character}{total}";
         }
     }
 }
